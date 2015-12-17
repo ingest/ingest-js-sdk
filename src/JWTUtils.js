52034c106f173c64cbd53f5b031544beb4116a50
Andy Moore <andy.moore@redspace.com>
2015-12-17 13:54:36 -0400

    INGEST-905 : Added JWT tools to check expiry on JWT tokensbefore making requests.

---
+++ src/JWTUtils.js
@@ -0,0 +1,61 @@
+/**
+ * UTILS for dealing with jwt tokens.
+ */
+
+/**
+ * Parse the payload out of the JWT token.
+ * @param  {string} token JWT Token.
+ * @return {object}       Parsed payload object.
+ */
+var parseTokenPayload = function (token) {
+  var parts = token.split('.');
+
+  // Return false if the token is invalid.
+  if (parts.length <= 1) {
+    return false;
+  }
+
+  // decode the payload portion.
+  var payload = window.atob(parts[1]);
+
+  // parse it as JSON
+  payload = JSON.parse(payload);
+
+  return payload;
+};
+
+/**
+ * Return true if the provided token has expired.
+ * @param  {string}  token JWT Token
+ * @return {Boolean}       True if expired.
+ */
+module.exports.isExpired = function (token) {
+  var data, exp, result, now;
+
+  data = parseTokenPayload(token);
+
+  result = true;
+
+  // Return true if the token does not parse properly.
+  if (!data) {
+    return result;
+  }
+
+  exp = data.exp;
+
+  // Get the unix timestamp in seconds.
+  now = new Date();
+  now = now.getTime() / 1000;
+
+  if (!exp) {
+    return result;
+  }
+
+  // If the expiry data is still in the future.
+  if (now < exp) {
+    result = false;
+  }
+
+  return result;
+
+};
