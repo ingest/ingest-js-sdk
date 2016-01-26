47ce8d6fbdc76f04f7ac75fa6657e12eb4f89add
Andy Moore <andy.moore@redspace.com>
2016-01-26 15:16:59 -0400

    INGEST-921 : Added util for completing a series of promises.

--- src/Utils.js
+++ src/Utils.js
@@ -46,4 +46,103 @@ Utils.promisify = function (state, value) {
   return promise;
 };
 
+/**
+ * Wrap an array of promises and return when they have all completed.
+ * @param   {Array}     promises  An array of promises to manage.
+ * @param   {boolean}   paused    A boolean to pause the execution of the promises.
+ * @return  {Promise}             A promise that is resolved when all of the promises have fulfilled.
+ */
+Utils.series = function (promises, paused) {
+
+  var promisesCount = promises.length;
+  var all = Promise();
+
+  var state = {
+    total: promisesCount,
+    complete: 0,
+    responses: [],
+    promises: promises,
+    paused: false
+  };
+
+  all.pause = Utils._seriesPause.bind(this, all, state);
+  all.resume = Utils._seriesResume.bind(this, all, state);
+
+  if (!paused) {
+    Utils._seriesCallPromise(promises[0], state, all);
+  }
+
+  return all;
+
+};
+
+/**
+ * Call a promise and pass the complete and error functions.
+ * @param  {Promise} promise Promise to execute on.
+ * @param  {Object} state   Persistent state of the series of promises.
+ */
+Utils._seriesCallPromise = function (promise, state, all) {
+  // call the promise;
+  if (!state.paused) {
+    promise().then(Utils._seriesComplete.bind(this, all, state),
+      Utils._seriesError.bind(this, all, state));
+  }
+};
+
+/**
+ * Manage the progress of the promises.
+ * @private
+ * @param  {Promise} all      All promise.
+ * @param  {Object}  state    State object used to persist the promise count.
+ * @param  {Object}  response Response of the promise being fulfilled.
+ */
+Utils._seriesComplete = function (all, state, response) {
+
+  // Increment the complete promises.
+  state.complete++;
+  state.responses.push(response);
+
+  if (state.complete >= state.total) {
+    all(true, state.responses);
+  } else {
+    if (!state.paused) {
+      // Execute the next promise.
+      Utils._seriesCallPromise(state.promises[state.complete], state, all);
+    }
+  }
+
+};
+
+/**
+ * Immediately reject the promise if there is an error.
+ * @param  {Promise} all  All promise.
+ * @param  {Object}  state    State object used to persist the promise count.
+ * @param  {Object}  response Response of the promise being fulfilled.
+ */
+Utils._seriesError = function (all, state, error) {
+  all(false, error);
+};
+
+/**
+ * Pause the current series of promises.
+ * @param  {Promise} all  All promise.
+ * @param  {Object}  state    State object used to persist the promise count.
+ */
+Utils._seriesPause = function (all, state) {
+  state.paused = true;
+};
+
+/**
+ * Resume the current series of promises.
+ * @param  {Promise} all  All promise.
+ * @param  {Object}  state    State object used to persist the promise count.
+ */
+Utils._seriesResume = function (all, state) {
+  state.paused = false;
+
+  if (state.complete !== state.total) {
+    Utils._seriesCallPromise(state.promises[state.complete], state, all);
+  }
+};
+
 module.exports = Utils;
