ce17412932c4195eb9cf00c923a6100d1e8d9578
Andy Moore <andy.moore@redspace.com>
2016-02-18 16:25:36 -0400

    INGEST-298 : Added proper support for partial updates. And ultimately we will not send the call to the server if the object hasn't actually changed.

--- src/Cache.js
+++ src/Cache.js
@@ -16,8 +16,8 @@ Cache.prototype._checkCacheSupport = function () {
   var support = 'support';
 
   try {
-    localStorage.setItem(support, support);
-    localStorage.removeItem(support);
+    window.localStorage.setItem(support, support);
+    window.localStorage.removeItem(support);
     return true;
   } catch (e) {
     return false;
@@ -37,13 +37,18 @@ Cache.prototype.retrieve = function (cacheKey) {
 
   try {
 
-    data = localStorage.getItem(cacheKey);
+    data = window.localStorage.getItem(cacheKey);
     data = JSON.parse(data);
+
+    if (!data) {
+      return value;
+    }
+
     value = data.value;
 
     if (data.expiry < Date.now()) {
       // Cache is expired;
-      localStorage.removeItem(cacheKey);
+      window.localStorage.removeItem(cacheKey);
       value = null;
     }
 
@@ -64,7 +69,7 @@ Cache.prototype.remove = function (cacheKey) {
   var result = true;
 
   try {
-    localStorage.removeItem(cacheKey);
+    window.localStorage.removeItem(cacheKey);
   } catch (error) {
     // Silent capture so that caching never gets in the way of returning a result.
     result = false;
@@ -92,7 +97,7 @@ Cache.prototype.save = function (cacheKey, value) {
 
     JSONResult = JSON.stringify(data);
 
-    localStorage.setItem(cacheKey, JSONResult);
+    window.localStorage.setItem(cacheKey, JSONResult);
 
   } catch (error) {
     // Silent capture so that caching never gets in the way of returning a result.
@@ -100,6 +105,107 @@ Cache.prototype.save = function (cacheKey, value) {
   }
 
   return result;
+};
+
+/**
+ * Return an object representing the differences between the provided object and the cached object.
+ * @example
+ * //'cachedObject' = {id: '12345'};
+ * cache.diff('cachedObject', {title: 'test'});
+ * // returns {title: 'test'}
+ * @example
+ * //'cachedObject' = {id: '12345', title: 'test'};
+ * cache.diff('cachedObject', {title: 'test2'});
+ * // returns {title: 'test2'}
+ *
+ * @param  {string} cacheKey  Cachekey of the object to compare against.
+ * @param  {object} item      Object to compare against the cached object.
+ * @param  {array}  forced    An array of properties to be returned in the resulting object regardless of difference.
+ * @return {object}           An object containing the differences.
+ */
+Cache.prototype.diff = function (cacheKey, item, forced) {
+  var property, properties, propertiesLength, i;
+  var force, forcedLength, n;
+  var original = this.retrieve(cacheKey);
+  var diff = null;
+
+  if (forced) {
+    forcedLength = forced.length;
+  }
+
+  properties = Object.keys(item);
+  propertiesLength = properties.length;
+
+  if (!original) {
+    // return the compare object if the original item wasn't found.
+    return item;
+  }
+
+  // Walk the object and compare the properties.
+  for (i = 0; i < propertiesLength; i++) {
+    property = properties[i];
+
+    // Check the values if both objects have the property.
+    if (original.hasOwnProperty(property)) {
+      if (item[property] === original[property]) {
+        // If the values are the same we don't need to add it to the diff so break.
+        continue;
+      }
+    }
+
+    // If the diff item is still null we need to create a new object to store the properties in.
+    if (!diff) {
+      diff = {};
+    }
+
+    // If we've made it this far the property is either a new one, or the item doesn't match.
+    diff[property] = item[property];
+
+  }
+
+  // As long as changes were found append the forced properties.
+  if (diff) {
+    for (n = 0; n < forcedLength; n++) {
+      property = forced[n];
+      diff[property] = item[property];
+    }
+  }
+
+  return diff;
+
+};
+
+/**
+ * Return an object representing the differences between the provided objects and the cached object.
+ * Similar to diff, but accepts an array of objects.
+ *
+ * @param  {string} cacheKey  Property on the provided objects that represents the cache key.
+ * @param  {object} item      Object to compare against the cached object.
+ * @param  {array}  forced    An array of properties to be returned in the resulting object regardless of difference.
+ * @return {object}           An object containing the differences.
+ */
+Cache.prototype.diffArray = function (cacheKey, items, forced) {
+
+  var i;
+  var itemsLength = items.length;
+  var item;
+  var result = null;
+  var results = [];
+
+  for (i = 0; i < itemsLength; i++) {
+    item = items[i];
+
+    result = this.diff(item[cacheKey], item, forced);
+
+    // Only add it to the result set if changes were found.
+    if (result !== null) {
+      results.push(result);
+    }
+
+  }
+
+  return results;
+
 };
 
 module.exports = Cache;
