b88dfd88553d30d839e01bfc829f58b9030cbb41
Andy Moore <andy.moore@redspace.com>
2016-03-09 14:56:14 -0400

    INGEST-1177 : Cleaning up the watch command so it runs properly, and doesn't have duplicate watchers.

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -5,18 +5,9 @@ var runSequence = require('run-sequence');
 // Watch for changes to the tests and source.
 gulp.task('watch', function (done) {
 
-  // On integration test change just execute those tests again.
-  gulp.watch(config.path.integrationSpec, function () {
-    runSequence(
-      'webpack:development',
-      'karma:once'
-    );
-  });
-
-  // On source change execute the whole build again.
   gulp.watch([
     config.path.src,
-    '!' + config.path.spec
+    config.path.spec
   ], function () {
     runSequence(
       'webpack:development',
