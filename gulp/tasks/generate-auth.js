d1f2b64dd7da330d0edc4018fb407ded64118a5d
Michael Cunningham <michael.cunningham@live.ca>
2016-06-22 09:33:54 -0300

    INGEST-1541: Reverting changes to authToken.js and disabling generate-auth gulp task

--- gulp/tasks/generate-auth.js
+++ gulp/tasks/generate-auth.js
@@ -1,58 +1,58 @@
-/*eslint no-console:false */
-var request = require('request');
-var fs = require('fs');
-var gulp = require('gulp');
+// /*eslint no-console:false */
+// var request = require('request');
+// var fs = require('fs');
+// var gulp = require('gulp');
 
-var authServer = 'http://weasley.teamspace.ad:3500/authorize?response_type=token&client_id=IngestLocal&state=xyz&scope=all&redirect_uri=http://local.weasley.teamspace.ad:7070';
+// var authServer = 'http://weasley.teamspace.ad:3500/authorize?response_type=token&client_id=IngestLocal&state=xyz&scope=all&redirect_uri=http://local.weasley.teamspace.ad:7070';
 
-// TODO, move sensitive data to ENV vars.
-var formData = {
-  network: 'fed6e925-dee4-41cc-be4a-479cabc149a5',  // UUID for `Redspace`
-  login: 'jamie.stackhouse@redspace.com',
-  password: 'password'
-};
+// // TODO, move sensitive data to ENV vars.
+// var formData = {
+//   network: 'fed6e925-dee4-41cc-be4a-479cabc149a5',  // UUID for `Redspace`
+//   login: 'jamie.stackhouse@redspace.com',
+//   password: 'password'
+// };
 
-gulp.task('test:generate-auth', function (done) {
+// gulp.task('test:generate-auth', function (done) {
 
-  var success = false;
+//   var success = false;
 
-  request.post({
-    url: authServer,
-    form: formData,
-    followRedirect: false
-  }, function (error, response, body) {
+//   request.post({
+//     url: authServer,
+//     form: formData,
+//     followRedirect: false
+//   }, function (error, response, body) {
 
-    if (error) {
-      throw new Error(error.stack);
-    }
+//     if (error) {
+//       throw new Error(error.stack);
+//     }
 
-    if (response.statusCode === 302) {
+//     if (response.statusCode === 302) {
 
-      if (typeof response.headers !== 'undefined' && response.headers.location !== 'undefined') {
+//       if (typeof response.headers !== 'undefined' && response.headers.location !== 'undefined') {
 
-        success = true;
+//         success = true;
 
-        var hashPos = response.headers.location.indexOf("#") + 1;
-        var hash = response.headers.location.substring(hashPos);
-        var params = hash.split('&');
-        var token = params[0].split('=')[1];
+//         var hashPos = response.headers.location.indexOf("#") + 1;
+//         var hash = response.headers.location.substring(hashPos);
+//         var params = hash.split('&');
+//         var token = params[0].split('=')[1];
 
-        fs.writeFile('authToken.js', 'window.token = "' + token + '";', function (error) {
+//         fs.writeFile('authToken.js', 'window.token = "' + token + '";', function (error) {
 
-          if (error) {
-            throw new Error(error.stack);
-          }
+//           if (error) {
+//             throw new Error(error.stack);
+//           }
 
-        });
+//         });
 
-      }
+//       }
 
-    }
+//     }
 
-    if (!success) {
-      throw new Error('There was an error while trying to generate an auth token.')
-    }
+//     if (!success) {
+//       throw new Error('There was an error while trying to generate an auth token.')
+//     }
 
-  });
+//   });
 
-});
+// });
