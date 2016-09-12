2a555e3b264c4e7f0688ff683f5a01db23c98cda
Michael Cunningham <michael.cunningham@redspace.com>
2016-09-12 11:47:58 -0300

    INGEST-1800: Updating Playlists resource to match changes in REST API.

--- src/resources/Playlists.js
+++ src/resources/Playlists.js
@@ -5,7 +5,10 @@ var extend = require('extend');
 
 function Playlists (options) {
 
-  var overrides = {};
+  var overrides = {
+    'playlistVideoById': '/<%=resource%>/<%=playlistId%>/video/<%=videoId%>',
+    'playlistReorder': '/<%=resource%>/<%=playlistId%>/reorder'
+  };
 
   options = extend(true, {}, overrides, options);
 
@@ -18,79 +21,122 @@ Playlists.prototype = Object.create(Resource.prototype);
 Playlists.prototype.constructor = Playlists;
 
 /**
- * Link the supplied video to the supplied playlist.
- * @param   {string}           playlistId       ID of the playlist to link the supplied video to.
- * @param   {object|array}     videos           A single video object, or an array of video objects.
- * @return  {promise}                           A promise which resolves when the request is complete.
+ * Add the supplied video to the supplied playlist.
+ * @param   {string}   playlistId  The UUID of the playlist to add the videoId to.
+ * @param   {string}   videoId     The UUID of the video to add to the playlist.
+ * @param   {number}   position    [Optional] Position of the new video in the playlist.
+ *                                 If omitted, the video in question will be added to the end of the given playlist.
+ * @return  {promise}              A promise which resolves when the request is complete.
  */
-Playlists.prototype.link = function (playlistId, video) {
-  return this._linkVideos(true, playlistId, video);
-};
+Playlists.prototype.addVideo = function (playlistId, videoId, position) {
+  var request, url, data;
 
-/**
- * Unlink the supplied video to the supplied playlist.
- * @param   {string}            playlistId      ID of the playlist to unlink the supplied video from.
- * @param   {object|array}      videos          A single video object, or an array of video objects.
- * @return  {promise}                           A promise which resolves when the request is complete.
- */
-Playlists.prototype.unlink = function (playlistId, video) {
-  return this._linkVideos(false, playlistId, video);
-};
-
-/**
- * Link or Unlink videos to a playlist.
- * @private
- * @param  {boolean}          link              A boolean indicating wether to link or unlink the item.
- * @param  {string}           playlistId        ID of the playlist to act upon.
- * @param  {object|array}     video             A single video object, or an array of video objects.
- * @return {promise}                            A promise which resolves when the request is complete.
- */
-Playlists.prototype._linkVideos = function (link, playlistId, video) {
-  var request;
-  var videosToLink = [];
-
-  if (typeof link !== 'boolean') {
+  if (typeof playlistId !== 'string' || typeof videoId !== 'string') {
     return utils.promisify(false,
-      'IngestAPI Playlists link requires a valid link flag passed as a boolean.');
+      'IngestAPI Playlists addVideo requires `playlistId` and `videoId` to both be strings.');
   }
 
-  if (typeof playlistId !== 'string') {
-    return utils.promisify(false,
-      'IngestAPI Playlists link requires a valid playlistId passed as a string.');
-  }
-
-  if (!video || (!Array.isArray(video) && typeof video !== 'object')) {
-    return utils.promisify(false,
-      'IngestAPI Playlists link requires a valid video passed as a valid object or array.');
-  }
-
-  // Check to see if we recieved either an object or an array.
-  if (!Array.isArray(video)) {
-    videosToLink.push(video);
-  } else {
-    videosToLink = video;
-  }
-
-  // Ensure that we have an array with a valid video.
-  if (videosToLink.length === 0) {
-    return utils.promisify(false,
-      'IngestAPI Playlists link requires at least one video to link.');
-  }
-
-  var url = utils.parseTokens(this.config.host + this.config.byId, {
+  url = utils.parseTokens(this.config.host + this.config.playlistVideoById, {
     resource: this.config.resource,
-    id: playlistId
+    playlistId: playlistId,
+    videoId: videoId
+  });
+
+  data = {};
+
+  if (typeof position === 'number') {
+    data.position = position;
+  }
+
+  request = new Request({
+    method: 'POST',
+    url: url,
+    token: this._tokenSource(),
+    data: data
+  });
+
+  return request.send().then(this._updateCachedResources.bind(this));
+};
+
+/**
+ * Remove the supplied video to the supplied playlist.
+ * @param   {string}   playlistId  The UUID of the playlist to add the videoId to.
+ * @param   {string}   videoId     The UUID of the video to add to the playlist.
+ * @param   {number}   position    Must be supplied as there can be multiple instances of the same video within a playlist.
+ * @return  {promise}              A promise which resolves when the request is complete.
+ */
+Playlists.prototype.removeVideo = function (playlistId, videoId, position) {
+  var request, url, data;
+
+  if (typeof playlistId !== 'string' || typeof videoId !== 'string') {
+    return utils.promisify(false,
+      'IngestAPI Playlists removeVideo requires `playlistId` and `videoId` to both be strings.');
+  }
+
+  if (typeof position !== 'number') {
+    return utils.promisify(false,
+      'IngestAPI Playlists removeVideo requires `position` to be a number');
+  }
+
+  data = {
+    position: position
+  };
+
+  url = utils.parseTokens(this.config.host + this.config.playlistVideoById, {
+    resource: this.config.resource,
+    playlistId: playlistId,
+    videoId: videoId
   });
 
   request = new Request({
-    method: link ? 'LINK' : 'UNLINK',
+    method: 'DELETE',
     url: url,
     token: this._tokenSource(),
-    data: videosToLink
+    data: data
   });
 
-  return request.send()
-          .then(this._updateCachedResources.bind(this));
+  return request.send().then(this._updateCachedResources.bind(this));
+
+};
+
+/**
+ * Re-orders a supplied playlist based on the given old and new positions.
+ * @param  {string}  playlistId   The UUID of the playlist to re-order.
+ * @param  {number}  oldPosition  The old position to move to the given new position.
+ * @param  {number}  newPosition  The new position to move to.
+ */
+Playlists.prototype.reorderVideo = function (playlistId, oldPosition, newPosition) {
+  var request, url, data;
+
+  if (typeof playlistId !== 'string') {
+    return utils.promisify(false,
+      'IngestAPI Playlists reorderVideo requires `playlistId` to be a string');
+  }
+
+  if (typeof oldPosition !== 'number' || typeof newPosition !== 'number') {
+    return utils.promisify(false,
+      'IngestAPI Playlists reorderVideo requires `oldPosition` and `newPosition` to be numbers.');
+  }
+
+  url = utils.parseTokens(this.config.host + this.config.playlistReorder, {
+    resource: this.config.resource,
+    playlistId: playlistId
+  });
+
+  data = {
+    old_position: oldPosition,
+    new_position: newPosition
+  };
+
+  request = new Request({
+    method: 'PUT',
+    url: url,
+    token: this._tokenSource(),
+    data: data
+  });
+
+  return request.send().then(this._updateCachedResources.bind(this));
+
 };
 
 module.exports = Playlists;
