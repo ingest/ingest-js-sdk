d8766c5e6c09c7999b7e2e5626406e5e795f3beb
Ashley Hasler <ahasler@hasler.teamspace.ad>
2017-05-15 10:04:52 -0300

    Added sorting functionality

--- src/resources/Events.js
+++ src/resources/Events.js
@@ -5,7 +5,14 @@ var Request = require('../Request');
 var utils = require('../Utils');
 var extend = require('extend');
 
-function Events () {
+function Events (options) {
+
+  var overrides = {
+    //?/events?filter=new,error,important,notifications&resource=videos,playlists,users,inputs,jobs
+    byType: '/<%=resource%>?filter=<%=input%>',
+  };
+
+  options = extend(true, {}, overrides, options);
 
   Resource.call(this, options);
 
@@ -14,5 +21,33 @@ function Events () {
 // This extends the base class of 'Resource'.
 Events.prototype = Object.create(Resource.prototype);
 Events.prototype.constructor = Events;
+
+/**
+ * Return a subset of items that match the filter terms.
+ * @param  {string}   input     The filter terms to match against.
+ * @param  {object}   headers   The headers to be passed to the request.
+ * @return {Promise}          A promise which resolves when the request is complete.
+ */
+Event.prototype.filter = function (input, headers) {
+  var url, request;
+
+  if (typeof input !== 'string') {
+    return utils.promisify(false,
+      'IngestAPI Event filtering requires input to be passed as a string.');
+  }
+
+  url = utils.parseTokens(this.config.host + this.config.filter, {
+    resource: this.config.resource,
+    input: encodeURIComponent(input)
+  });
+
+  request = new Request({
+    url: url,
+    token: this._tokenSource(),
+    headers: headers
+  });
+
+  return request.send();
+};
 
 module.exports = Events;
